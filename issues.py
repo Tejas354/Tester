9ae179a9b87711c1843742dac154a776e3d543e1
SathvikaMondu <123160305+SathvikaMondu@users.noreply.github.com>
2024-05-28 10:14:29 +0530

    Commit with deactivated API key

--- issues.py
+++ issues.py
@@ -7,20 +7,6 @@ match = re.match(r"^([a-z]{2}-[a-z]+-\d{1}).*", self.region)
 #     print(i)
 #     print('************')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #     print('************')
 class Rule(object):
     def __init__(self,name:str, status:str, **kwargs):
